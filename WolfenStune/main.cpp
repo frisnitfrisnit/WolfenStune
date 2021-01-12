20689b9aaad9ebb5b2c31675ed0ea1c99fb38cc2
frisnitfrisnit <frisnit@hotmail.com>
2021-01-12 01:02:31 +0000

    Initial Commit

---
+++ WolfenStune/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WolfenStune
+//
+//  Created by Stuart Hunt on 12/01/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
